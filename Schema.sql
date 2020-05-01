9241b03c683704895540bd6e9cd2669f7b28c0dd
Jorge Castello <jorgio.castello@gmail.com>
2020-05-01 00:05:09 -0700

    Update Schema

--- Schema.sql
+++ Schema.sql
@@ -6,26 +6,33 @@ USE tripAdvisorGallery;
 
 CREATE TABLE activity (
   id INT NOT NULL auto_increment,
-  NAME VARCHAR(255) NOT NULL,
-  LOCATION VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  location VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE reviews (
   id INT NOT NULL AUTO_INCREMENT,
-  user VARCHAR(50)
+  user VARCHAR(50),
   user_contributions INT,
   date_created DATE,
   review_text TEXT,
   review_stars INT,
-  helpful_score INT
+  helpful_score INT,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE photos (
   id INT NOT NULL AUTO_INCREMENT,
+  link TEXT,
   alt TEXT NOT NULL,
-  activity_id INT NOT NULL FOREIGN KEY REFERENCES activity(id),
-  reviews_id INT FOREIGN KEY REFERENCES reviews(id)
+  PRIMARY KEY (id),
+
+  activity_id INT NOT NULL,
+  CONSTRAINT fk_activity_id FOREIGN KEY (activity_id) REFERENCES activity(id),
+
+  review_id INT,
+  CONSTRAINT fk_review_id FOREIGN KEY (review_id) REFERENCES reviews(id)
 );
 
 
