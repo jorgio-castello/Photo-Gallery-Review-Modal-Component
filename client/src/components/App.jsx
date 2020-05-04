dee34b395db04a2be0fb4ba7177bdc22eb36a152
Jorge Castello <jorgio.castello@gmail.com>
2020-05-04 11:19:41 -0700

    Reset git cache

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import ExampleActivityData from '../../../ExampleActivityData';
+
+const { activity, photos } = ExampleActivityData;
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      activePhoto: {},
+      photos: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      activePhoto: photos[0],
+      photos,
+    }, () => console.log(this.state));
   }
 
   render() {
+    const { link, alt } = this.state.activePhoto;
+
     return (
-      <div>This is a test</div>
+      <img src={`https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/${link}`} alt={alt} />
     );
   }
 }
