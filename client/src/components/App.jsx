9e85633908c379254f470b1e611964ea632fb514
Jorge Castello <jorgio.castello@gmail.com>
2020-05-04 13:42:40 -0700

    Working on Slider component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,26 +2,46 @@ import React from 'react';
 import ExampleActivityData from '../../../ExampleActivityData';
 import ImageSliderCSS from '../style/ImageSlider.css';
 
-const { activity, photos } = ExampleActivityData;
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activePhoto: {},
-      photos: []
+      activePhotoIdx: 0,
+      photos: ExampleActivityData.photos,
     };
+    this.nextImageHandler = this.nextImageHandler.bind(this);
+    this.prevImageHandler = this.prevImageHandler.bind(this);
   }
 
   componentDidMount() {
+    const { photos } = ExampleActivityData;
     this.setState({
-      activePhoto: photos[0],
+      activePhotoIdx: 0,
       photos,
-    }, () => console.log(this.state));
+    });
+  }
+
+  nextImageHandler() {
+    const { activePhotoIdx, photos } = this.state;
+    if (activePhotoIdx !== photos.length - 1) {
+      this.setState({
+        activePhotoIdx: activePhotoIdx + 1,
+      });
+    }
+  }
+
+  prevImageHandler() {
+    const { activePhotoIdx } = this.state;
+    if (activePhotoIdx !== 0) {
+      this.setState({
+        activePhotoIdx: activePhotoIdx - 1,
+      });
+    }
   }
 
   render() {
-    const { activePhoto: { link, alt } } = this.state;
+    const { photos, activePhotoIdx } = this.state;
+    const { link, alt } = photos[activePhotoIdx];
     const imageStyle = {
       backgroundImage: `url(https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/${link})`,
       backgroundPosition: 'center',
@@ -30,11 +50,21 @@ class App extends React.Component {
 
     return (
       <div style={imageStyle} className={ImageSliderCSS.container} aria-label={alt}>
-        <span className={ImageSliderCSS.prev_button}>Prev</span>
-        <span className={ImageSliderCSS.next_button}>Next</span>
+        <span
+          onClick={this.prevImageHandler}
+          className={ImageSliderCSS.prev_button}
+        >
+          Prev
+        </span>
+        <span
+          onClick={this.nextImageHandler}
+          className={ImageSliderCSS.next_button}
+        >
+          Next
+        </span>
         <span className={ImageSliderCSS.view_all_button}>
           View All Photos (
-          {this.state.photos.length}
+          {photos.length}
           )
         </span>
       </div>
