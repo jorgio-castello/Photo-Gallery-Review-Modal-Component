8aa5bf41e0dbee48a2eb8164835e4dab539928e7
Jorge Castello <jorgio.castello@gmail.com>
2020-05-05 10:59:06 -0700

    Import ImageSlider from components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
+// eslint-disable-next-line import/extensions
+import ImageSlider from './ImageSlider.jsx';
 
 // Data
 import ExampleActivityData from '../../../ExampleActivityData';
-
 // Helper Functions
 import preloadImages from '../helpers/preloadImages';
-import handlers from '../helpers/handlers';
-
-// CSS
-import ImageSliderCSS from '../style/ImageSlider.css';
-
-// Event Handlers
-const { nextImageHandler, prevImageHandler } = handlers;
+// Import Event Handlers
+import eventHandlers from '../helpers/handlers';
+// Destructure Specific Handlers
+const { nextImageHandler, prevImageHandler } = eventHandlers;
 
 
 class App extends React.Component {
@@ -38,32 +36,16 @@ class App extends React.Component {
   render() {
     const { photos, activePhotoIdx } = this.state;
     const { link, alt } = photos[activePhotoIdx];
-    const imageStyle = {
-      backgroundImage: `url(https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/${link})`,
-      backgroundPosition: 'center',
-      backgroundSize: 'cover',
-    };
+    const imageStyle = { backgroundImage: `url(https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/${link})` };
 
     return (
-      <div style={imageStyle} className={ImageSliderCSS.container} aria-label={alt}>
-        <span
-          onClick={this.prevImageHandler}
-          className={ImageSliderCSS.prev_button}
-        >
-          Prev
-        </span>
-        <span
-          onClick={this.nextImageHandler}
-          className={ImageSliderCSS.next_button}
-        >
-          Next
-        </span>
-        <span className={ImageSliderCSS.view_all_button}>
-          View All Photos (
-          {photos.length}
-          )
-        </span>
-      </div>
+      <ImageSlider
+        backgroundImage={imageStyle}
+        alt={alt}
+        count={photos.length}
+        prevImageHandler={this.prevImageHandler}
+        nextImageHandler={this.nextImageHandler}
+      />
     );
   }
 }
