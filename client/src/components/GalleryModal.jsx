93f5b873a9f846940f605054b1c1a37d8939f40f
Jorge Castello <jorgio.castello@gmail.com>
2020-05-07 09:28:30 -0700

    Refactor Modal Header into its own component

--- client/src/components/GalleryModal.jsx
+++ client/src/components/GalleryModal.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import GalleryModalCSS from '../style/GalleryModal.css';
+import ModalHeader from './ModalHeader';
 import GalleryModalPicture from './GalleryModalPicture';
 
 // eslint-disable-next-line object-curly-newline
@@ -7,27 +8,7 @@ const GalleryModal = ({ name, location, photos, updateGalleryDisplay }) => (
   <>
     <div className={GalleryModalCSS.modalBackground}>
       <div className={GalleryModalCSS.modalContent}>
-        <div className={GalleryModalCSS.modalHeader}>
-          Photos of
-          {' '}
-          {name}
-          {' '}
-          from
-          {' '}
-          {location}
-          <button
-            type="button"
-            className={GalleryModalCSS.exit}
-            onClick={updateGalleryDisplay}
-          >
-            <img
-              alt="Close"
-              src="https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/Icons/cross-icon.png"
-              height="20px"
-              className={GalleryModalCSS.exitButton}
-            />
-          </button>
-        </div>
+        <ModalHeader name={name} location={location} updateGalleryDisplay={updateGalleryDisplay} />
         <div className={GalleryModalCSS.modalGallery}>
           {photos.map((photo, index) => <GalleryModalPicture photo={photo} key={index} />)}
         </div>
