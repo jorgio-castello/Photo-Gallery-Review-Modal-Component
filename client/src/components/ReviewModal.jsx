2acaf0d8e9be2b92070a6d0287d6dd921c4c1efe
Jorge Castello <jorgio.castello@gmail.com>
2020-05-07 19:07:10 -0700

    Add directional buttons to ReviewModal

--- client/src/components/ReviewModal.jsx
+++ client/src/components/ReviewModal.jsx
@@ -6,11 +6,33 @@ const ReviewModal = ({photos, activePhotoIdx}) => {
 
   return (
     <div className={ReviewModalCSS.container}>
+      <div className={ReviewModalCSS.leftContainer}>
+        <div className={ReviewModalCSS.prev_button}>
+          <button
+            type="button"
+            className={ReviewModalCSS.slider_button}
+            // onClick={this.handleClick}
+          >
+            <img className={ReviewModalCSS.directional_arrow_left} src="https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/Icons/angle-arrow-down.png" alt="Previous" />
+          </button>
+        </div>
+      </div>
       <img
         className={ReviewModalCSS.image}
         alt={activePhoto.alt}
         src={`https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/${activePhoto.link}`}
       />
+      <div className={ReviewModalCSS.rightContainer}>
+        <div className={ReviewModalCSS.next_button}>
+          <button
+            type="button"
+            className={ReviewModalCSS.slider_button}
+            // onClick={this.handleClick}
+          >
+            <img className={ReviewModalCSS.directional_arrow_right} src="https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/Icons/angle-arrow-down.png" alt="Next" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
