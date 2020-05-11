866e039aa509fd28b8a6b1344eec4558ebd49a5b
Jorge Castello <jorgio.castello@gmail.com>
2020-05-10 17:44:24 -0700

    Modal.jsx - add comments and fix eslint issues

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,21 +1,69 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import ModalCSS from '../style/Modal.css';
 import ModalHeader from './ModalHeader';
 import GalleryModal from './GalleryModal';
 import ReviewModal from './ReviewModal';
 
-// eslint-disable-next-line object-curly-newline
-const Modal = ({ name, location, activePhotoIdx, photos, updateGalleryDisplay, shouldShowGalleryModal, handleImageSliderClick, showGalleryModal, showReviewModal }) => (
+const Modal = (
+  {
+    name, location, activePhotoIdx, photos, // Props related to activity / photo data
+    shouldShowGalleryModal, // Boolean to show either gallery or review modal
+    updateGalleryDisplay, handleImageSliderClick, showGalleryModal, showReviewModal, // Event funcs
+  },
+) => (
   <>
     <div className={ModalCSS.modalBackground}>
       <div className={ModalCSS.modalContent}>
-        <ModalHeader name={name} location={location} updateGalleryDisplay={updateGalleryDisplay} />
-        {shouldShowGalleryModal
-          ? <GalleryModal photos={photos} showReviewModal={showReviewModal} />
-          : <ReviewModal photos={photos} activePhotoIdx={activePhotoIdx} handleImageSliderClick={handleImageSliderClick} showGalleryModal={showGalleryModal} />}
+        <ModalHeader
+          name={name}
+          location={location}
+          updateGalleryDisplay={updateGalleryDisplay}
+        />
+
+        {shouldShowGalleryModal // Ternary test to show appropriate modal
+          ? (
+            <GalleryModal
+              photos={photos}
+              showReviewModal={showReviewModal}
+            />
+          )
+          : (
+            <ReviewModal
+              photos={photos}
+              activePhotoIdx={activePhotoIdx}
+              handleImageSliderClick={handleImageSliderClick}
+              showGalleryModal={showGalleryModal}
+            />
+          )}
       </div>
     </div>
   </>
 );
+
+Modal.defaultProps = {
+  name: '',
+  location: '',
+  activePhotoIdx: 0,
+  photos: [],
+  updateGalleryDisplay: () => {},
+  shouldShowGalleryModal: false,
+  handleImageSliderClick: () => {},
+  showGalleryModal: () => {},
+  showReviewModal: () => {},
+};
+
+Modal.propTypes = {
+  name: PropTypes.string,
+  location: PropTypes.string,
+  activePhotoIdx: PropTypes.number,
+  photos: PropTypes.shape([]),
+  updateGalleryDisplay: PropTypes.func,
+  shouldShowGalleryModal: PropTypes.bool,
+  handleImageSliderClick: PropTypes.func,
+  showGalleryModal: PropTypes.func,
+  showReviewModal: PropTypes.func,
+};
 
 export default Modal;
