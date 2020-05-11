7cdeaaa2600f0a72b1c265645538bffdad0aeb9b
Jorge Castello <jorgio.castello@gmail.com>
2020-05-10 18:22:50 -0700

    Add awsBaseUrl

--- AmazonS3Links.js
+++ AmazonS3Links.js
@@ -1,4 +1,5 @@
 export default {
   arrowIcon: 'https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/Icons/angle-arrow-down.png',
+  awsBaseUrl: 'https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com',
   cameraIcon: 'https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/Icons/photo-camera.png',
 };
