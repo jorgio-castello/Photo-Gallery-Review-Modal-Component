50991cd409faf6abbbd4f6d238da312b6b7c8509
Jorge Castello <jorgio.castello@gmail.com>
2020-05-10 19:14:10 -0700

    Refactor ReviewModal component to use containers

--- client/src/components/ReviewModal/index.jsx
+++ client/src/components/ReviewModal/index.jsx
@@ -1,78 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import ReviewModalCSS from '../../style/ReviewModal.css';
+// Child Components
+import ContainerLeft from './childComponents/ContainerLeft';
+import ContainerRight from './childComponents/ContainerRight';
 import Review from '../Review';
 
+// CSS styling
+import ReviewModalCSS from '../../style/ReviewModal.css';
+
+// AWS links' data
 import awsS3Links from '../../../../AmazonS3Links';
+// Destructure baseurl from AWS links
+const { awsBaseUrl } = awsS3Links;
 
-const { arrowIcon, awsBaseUrl } = awsS3Links;
-
+// -------------------------------------------------------------------------------------------------
 const ReviewModal = (
   {
-    photos, activePhotoIdx, handleImageSliderClick, showGalleryModal,
+    photos, activePhotoIdx, // array of photos, and the active photoIdx
+    handleImageSliderClick, showGalleryModal, // Handlers to change active image, show gallery modal
   },
 ) => {
   const activePhoto = photos[activePhotoIdx];
 
   return (
     <div className={ReviewModalCSS.container}>
-      <div className={ReviewModalCSS.leftContainer}>
-        <div className={ReviewModalCSS.showGalleryContainer}>
-          <button
-            type="button"
-            className={ReviewModalCSS.showGalleryButton}
-            onClick={showGalleryModal}
-          >
-            Gallery
-          </button>
-        </div>
-        <div className={ReviewModalCSS.prev_button}>
-          <button
-            type="button"
-            className={ReviewModalCSS.slider_button}
-            onClick={handleImageSliderClick}
-          >
-            <img
-              className={ReviewModalCSS.directional_arrow_left}
-              src={arrowIcon}
-              alt="Previous"
-            />
-          </button>
-        </div>
-      </div>
+
+      <ContainerLeft // Contains buttons on left-side of the ReviewModal component
+        showGalleryModal={showGalleryModal}
+        handleImageSliderClick={handleImageSliderClick}
+      />
+
+      {/* Primary content of the page: current image */}
       <img
         className={ReviewModalCSS.image}
         alt={activePhoto.alt}
         src={`${awsBaseUrl}/${activePhoto.link}`}
       />
-      <div className={ReviewModalCSS.rightContainer}>
-        <div className={ReviewModalCSS.showStatsContainer}>
-          <span className={ReviewModalCSS.activityPhotoStats}>
-            {Number(activePhotoIdx) + 1}
-            {' '}
-            of
-            {' '}
-            {photos.length}
-          </span>
-        </div>
-        <div className={ReviewModalCSS.next_button}>
-          <button
-            type="button"
-            className={ReviewModalCSS.slider_button}
-            onClick={handleImageSliderClick}
-          >
-            <img
-              className={ReviewModalCSS.directional_arrow_right}
-              src={arrowIcon}
-              alt="Next"
-            />
-          </button>
-        </div>
-      </div>
+
+      <ContainerRight // Contains information / buttons on right-side of the ReviewModal component
+        activePhotoIdx={activePhotoIdx}
+        photos={photos}
+        handleImageSliderClick={handleImageSliderClick}
+      />
+
+      {/* Displays relevant information about the review associated with this phot */}
       <div className={ReviewModalCSS.reviewComponent}>
         <Review photos={photos} activePhotoIdx={activePhotoIdx} />
       </div>
+
     </div>
   );
 };
