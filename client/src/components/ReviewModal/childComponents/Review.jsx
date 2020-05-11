ca8ba5f1058a08e389fff3124a86638537bbbdb2
Jorge Castello <jorgio.castello@gmail.com>
2020-05-10 21:23:49 -0700

    Refactor Review.jsx to utilize child components

--- client/src/components/ReviewModal/childComponents/Review.jsx
+++ client/src/components/ReviewModal/childComponents/Review.jsx
@@ -1,26 +1,20 @@
+/* eslint-disable camelcase */
 import React from 'react';
 import ReviewCSS from '../../../style/Review.css';
+import Emblem from './Emblem';
+import StarsAndDescription from './StarsAndDescription';
+import ReviewFooter from './ReviewFooter';
+
+import reviewHelpers from '../Helpers';
+
+const { setUsernameAndLetter, checkForReview } = reviewHelpers;
 
 class Review extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      showReview: false,
-    };
+    this.setUsernameAndLetter = setUsernameAndLetter;
+    this.checkForReview = checkForReview;
     this.handleReadReviewClick = this.handleReadReviewClick.bind(this);
-  }
-
-  setUsernameAndLetter() {
-    const { photos, activePhotoIdx } = this.props;
-    const activePhoto = photos[activePhotoIdx];
-
-    const { user } = activePhoto;
-    let firstLetter = '';
-    if (user) {
-      firstLetter = user.slice(0, 1);
-    }
-
-    return [user, firstLetter];
   }
 
   handleReadReviewClick() {
@@ -30,72 +24,36 @@ class Review extends React.Component {
     });
   }
 
-
-  checkForReview() {
-    const { photos, activePhotoIdx } = this.props;
-    const activePhoto = photos[activePhotoIdx];
-
-    let {review_title, review_description, review_helpful_score, review_stars} = activePhoto;
-    if (review_title === "null") {
-      review_title = null;
-    }
-    if (review_description === "null") {
-      review_description = null;
-    }
-
-    return [review_title, review_description, review_helpful_score, review_stars];
-  }
-
   render() {
-    const [user, firstLetter] = this.setUsernameAndLetter();
-    const [review_title, review_description, review_helpful_score, review_stars] = this.checkForReview();
-    const { showReview } = this.state;
+    const [user, firstLetter] = this.setUsernameAndLetter(this.props);
+    const [
+      review_title, review_description, review_helpful_score, review_stars,
+    ] = this.checkForReview(this.props);
 
     return (
       <div className={ReviewCSS.container}>
-        <div className={ReviewCSS.emblem}><span className={ReviewCSS.emblemLetter}>{firstLetter}</span></div>
-        <div className={ReviewCSS.username}>{user}</div>
-        {review_title && !review_description ? <div className={ReviewCSS.title}>{review_title}</div> : <div />}
+        <Emblem firstLetter={firstLetter} user={user} />
+
+        {/* Display content specific for review w/ title and w/out description */}
+        {/* This scenario occurs when management / a user posts an image without a description */}
+        {review_title && !review_description
+          ? <div className={ReviewCSS.title}>{review_title}</div>
+          : <div />}
+
+        {/* Displays the review's description if it exists */}
         {review_description
           ? (
-            <>
-              <div className={ReviewCSS.starsAndDescriptionContainer}>
-                <div className={ReviewCSS.starsContainer}>
-                  <div className={ReviewCSS.star} />
-                  <div className={ReviewCSS.star} />
-                  <div className={ReviewCSS.star} />
-                  <div className={ReviewCSS.star} />
-                  <div className={ReviewCSS.star} />
-                </div>
-                <div className={ReviewCSS.descriptionShort}>
-                  {`${review_description.slice(0, 150)}...`}
-                </div>
-              </div>
-              <button onClick={this.handleReadReviewClick} className={ReviewCSS.readReviewButton} type="button">Read Review</button>
-            </>
+            <StarsAndDescription
+              stars={review_stars}
+              description={review_description}
+              handleReadReviewClick={this.handleReadReviewClick}
+            />
           )
           : <div />}
+
+        {/* Displays the review's helpful score if it exists */}
         {review_helpful_score
-          ? (
-            <div className={ReviewCSS.footer}>
-              <div className={ReviewCSS.footerContent}>
-                <button
-                  type="button"
-                  className={ReviewCSS.helpfulScore}
-                >
-                  {/* <img className={ReviewCSS.thumbs_up} alt="Thumbs Up" src="https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/Icons/hands.png" /> */}
-                  Helpful (
-                    {review_helpful_score}
-                  )
-                </button>
-                <div className={ReviewCSS.footerContentRight}>
-                  <button type="button" className={ReviewCSS.saveTrip}>SaveTrip</button>
-                  <button type="button" className={ReviewCSS.reportPhoto}>ReportPhoto</button>
-                </div>
-                <div className={ReviewCSS.footerActions} />
-              </div>
-            </div>
-          )
+          ? <ReviewFooter helpfulScore={review_helpful_score} />
           : <div />}
       </div>
     );
