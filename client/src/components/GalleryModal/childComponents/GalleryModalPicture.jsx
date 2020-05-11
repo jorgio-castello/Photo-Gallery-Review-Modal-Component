bf546f242f34d425eab468f42f9e1fa427c5f5f5
Jorge Castello <jorgio.castello@gmail.com>
2020-05-10 18:23:11 -0700

    Add comments, fix eslint issues

--- client/src/components/GalleryModal/childComponents/GalleryModalPicture.jsx
+++ client/src/components/GalleryModal/childComponents/GalleryModalPicture.jsx
@@ -1,18 +1,42 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import GalleryModalPictureCSS from '../../../style/GalleryModalPicture.css';
+import awsS3Links from '../../../../../AmazonS3Links';
+
+const { awsBaseUrl } = awsS3Links;
 
 const GalleryModalPicture = ({ photo, showReviewModal, id }) => (
+  // *Accessibility feature to add: keypress for view reviewModal
   <div className={GalleryModalPictureCSS.container}>
+    {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
     <img
       alt={photo.alt}
-      src={`https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/${photo.link}`}
+      src={`${awsBaseUrl}/${photo.link}`}
       onClick={showReviewModal}
       id={id}
       className={GalleryModalPictureCSS.photoStyling}
+      onKeyPress={() => {}}
     />
   </div>
 );
+
+GalleryModalPicture.defaultProps = {
+  photo: {
+    alt: '',
+    link: '',
+  },
+  showReviewModal: () => {},
+  id: 0,
+};
+
+GalleryModalPicture.propTypes = {
+  photo: {
+    alt: PropTypes.string,
+    link: PropTypes.string,
+  },
+  showReviewModal: PropTypes.func,
+  id: PropTypes.number,
+};
 
 export default GalleryModalPicture;
