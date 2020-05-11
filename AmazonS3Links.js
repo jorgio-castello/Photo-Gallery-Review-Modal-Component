90e971f750d4590835edbe7534884eedf84242df
Jorge Castello <jorgio.castello@gmail.com>
2020-05-10 17:21:56 -0700

    Create AWS S3 file to hold links to img assets

---
+++ AmazonS3Links.js
@@ -0,0 +1,4 @@
+export default {
+  arrowIcon: 'https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/Icons/angle-arrow-down.png',
+  cameraIcon: 'https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/Icons/photo-camera.png',
+};
