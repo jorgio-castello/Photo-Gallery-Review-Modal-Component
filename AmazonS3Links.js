90c00b7a439f47bbf841fee4dd6b2e5aaafca533
Jorge Castello <jorgio.castello@gmail.com>
2020-05-10 19:26:26 -0700

    Add crossIcon to AWS links file

--- AmazonS3Links.js
+++ AmazonS3Links.js
@@ -2,4 +2,5 @@ export default {
   arrowIcon: 'https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/Icons/angle-arrow-down.png',
   awsBaseUrl: 'https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com',
   cameraIcon: 'https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/Icons/photo-camera.png',
+  crossIcon: 'https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/Icons/cross-icon.png',
 };
