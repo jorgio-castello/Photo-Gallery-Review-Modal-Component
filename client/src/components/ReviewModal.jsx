3968744d95d73c4295d94630db73a00333ae03c2
Jorge Castello <jorgio.castello@gmail.com>
2020-05-10 18:42:07 -0700

    ReviewModal - add comments, fix eslint issues

--- client/src/components/ReviewModal.jsx
+++ client/src/components/ReviewModal.jsx
@@ -1,63 +1,94 @@
 import React from 'react';
-import ReviewModalCSS from '../style/ReviewModal.css';
+import PropTypes from 'prop-types';
 
+import ReviewModalCSS from '../style/ReviewModal.css';
 import Review from './Review';
 
-const ReviewModal = ({photos, activePhotoIdx, handleImageSliderClick, showGalleryModal}) => {
+import awsS3Links from '../../../AmazonS3Links';
+
+const { arrowIcon, awsBaseUrl } = awsS3Links;
+
+const ReviewModal = (
+  {
+    photos, activePhotoIdx, handleImageSliderClick, showGalleryModal,
+  },
+) => {
   const activePhoto = photos[activePhotoIdx];
 
   return (
-      <div className={ReviewModalCSS.container}>
-        <div className={ReviewModalCSS.leftContainer}>
-          <div className={ReviewModalCSS.showGalleryContainer}>
-            <button
-              type="button"
-              className={ReviewModalCSS.showGalleryButton}
-              onClick={showGalleryModal}
-            >
-              Gallery
-            </button>
-          </div>
-          <div className={ReviewModalCSS.prev_button}>
-            <button
-              type="button"
-              className={ReviewModalCSS.slider_button}
-              onClick={handleImageSliderClick}
-            >
-              <img className={ReviewModalCSS.directional_arrow_left} src="https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/Icons/angle-arrow-down.png" alt="Previous" />
-            </button>
-          </div>
+    <div className={ReviewModalCSS.container}>
+      <div className={ReviewModalCSS.leftContainer}>
+        <div className={ReviewModalCSS.showGalleryContainer}>
+          <button
+            type="button"
+            className={ReviewModalCSS.showGalleryButton}
+            onClick={showGalleryModal}
+          >
+            Gallery
+          </button>
         </div>
-        <img
-          className={ReviewModalCSS.image}
-          alt={activePhoto.alt}
-          src={`https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/${activePhoto.link}`}
-        />
-        <div className={ReviewModalCSS.rightContainer}>
-          <div className={ReviewModalCSS.showStatsContainer}>
-            <span className={ReviewModalCSS.activityPhotoStats}>
-              {Number(activePhotoIdx) + 1}
-              {' '}
-              of
-              {' '}
-              {photos.length}
-            </span>
-          </div>
-          <div className={ReviewModalCSS.next_button}>
-            <button
-              type="button"
-              className={ReviewModalCSS.slider_button}
-              onClick={handleImageSliderClick}
-            >
-              <img className={ReviewModalCSS.directional_arrow_right} src="https://trip-advisor-photo-gallery.s3-us-west-1.amazonaws.com/Icons/angle-arrow-down.png" alt="Next" />
-            </button>
-          </div>
-        </div>
-        <div className={ReviewModalCSS.reviewComponent}>
-          <Review photos={photos} activePhotoIdx={activePhotoIdx} />
+        <div className={ReviewModalCSS.prev_button}>
+          <button
+            type="button"
+            className={ReviewModalCSS.slider_button}
+            onClick={handleImageSliderClick}
+          >
+            <img
+              className={ReviewModalCSS.directional_arrow_left}
+              src={arrowIcon}
+              alt="Previous"
+            />
+          </button>
         </div>
       </div>
+      <img
+        className={ReviewModalCSS.image}
+        alt={activePhoto.alt}
+        src={`${awsBaseUrl}/${activePhoto.link}`}
+      />
+      <div className={ReviewModalCSS.rightContainer}>
+        <div className={ReviewModalCSS.showStatsContainer}>
+          <span className={ReviewModalCSS.activityPhotoStats}>
+            {Number(activePhotoIdx) + 1}
+            {' '}
+            of
+            {' '}
+            {photos.length}
+          </span>
+        </div>
+        <div className={ReviewModalCSS.next_button}>
+          <button
+            type="button"
+            className={ReviewModalCSS.slider_button}
+            onClick={handleImageSliderClick}
+          >
+            <img
+              className={ReviewModalCSS.directional_arrow_right}
+              src={arrowIcon}
+              alt="Next"
+            />
+          </button>
+        </div>
+      </div>
+      <div className={ReviewModalCSS.reviewComponent}>
+        <Review photos={photos} activePhotoIdx={activePhotoIdx} />
+      </div>
+    </div>
   );
+};
+
+ReviewModal.defaultProps = {
+  photos: [],
+  activePhotoIdx: 0,
+  handleImageSliderClick: () => {},
+  showGalleryModal: () => {},
+};
+
+ReviewModal.propTypes = {
+  photos: PropTypes.shape([]),
+  activePhotoIdx: PropTypes.number,
+  handleImageSliderClick: PropTypes.func,
+  showGalleryModal: PropTypes.func,
 };
 
 export default ReviewModal;
