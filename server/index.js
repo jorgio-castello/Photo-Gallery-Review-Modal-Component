179f7b7eff3d0340b5ab15623601e7aa78d92c2a
Jorge Castello <jorgio.castello@gmail.com>
2020-05-15 14:28:50 -0700

    Incorporate morgan into server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const morgan = require('morgan');
 const { getGalleryForActivityId } = require('./controllers');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/tripAdvisor/:activityId/gallery', getGalleryForActivityId);
