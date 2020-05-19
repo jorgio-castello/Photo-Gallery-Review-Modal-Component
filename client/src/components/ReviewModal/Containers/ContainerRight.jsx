ce95f09b82c718c23aedda489794bcef19185e38
Jorge Castello <jorgio.castello@gmail.com>
2020-05-19 01:13:16 -0700

    Re-introduce prop-types to Container Right

--- client/src/components/ReviewModal/Containers/ContainerRight.jsx
+++ client/src/components/ReviewModal/Containers/ContainerRight.jsx
@@ -33,8 +33,17 @@ const ContainerRight = ({ activePhotoIdx, photos, handleImageSliderClick }) => (
   </div>
 );
 
-ContainerRight.defaultProps = {};
+ContainerRight.defaultProps = {
+  photos: [],
+  activePhotoIdx: 0,
+  handleImageSliderClick: () => {},
+};
 
-ContainerRight.propTypes = {};
+ContainerRight.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  photos: PropTypes.array,
+  activePhotoIdx: PropTypes.number,
+  handleImageSliderClick: PropTypes.func,
+};
 
 export default ContainerRight;
