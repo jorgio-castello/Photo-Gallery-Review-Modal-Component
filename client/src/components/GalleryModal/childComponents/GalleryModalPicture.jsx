6954c49995c0a3edd9a8af19bd30e0c05584b31d
Jorge Castello <jorgio.castello@gmail.com>
2020-05-19 01:09:32 -0700

    Wrap GalleryModalPicture in a React Fragment

--- client/src/components/GalleryModal/childComponents/GalleryModalPicture.jsx
+++ client/src/components/GalleryModal/childComponents/GalleryModalPicture.jsx
@@ -8,21 +8,32 @@ const { awsBaseUrl } = awsS3Links;
 
 const GalleryModalPicture = ({ photo, showReviewModal, id }) => (
   // *Accessibility feature to add: keypress for view reviewModal
-  <div className={GalleryModalPictureCSS.container}>
-    {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
-    <img
-      alt={photo.alt}
-      src={`${awsBaseUrl}/${photo.link}`}
-      onClick={showReviewModal}
-      id={id}
-      className={GalleryModalPictureCSS.photoStyling}
-      onKeyPress={() => {}}
-    />
-  </div>
+  <>
+    <div className={GalleryModalPictureCSS.container}>
+      {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
+      <img
+        alt={photo.alt}
+        src={`${awsBaseUrl}/${photo.link}`}
+        onClick={showReviewModal}
+        id={id}
+        className={GalleryModalPictureCSS.photoStyling}
+        onKeyPress={() => {}}
+      />
+    </div>
+  </>
 );
 
-GalleryModalPicture.defaultProps = {};
+GalleryModalPicture.defaultProps = {
+  photo: {},
+  showReviewModal: () => {},
+  id: 0,
+};
 
-GalleryModalPicture.propTypes = {};
+GalleryModalPicture.propTypes = {
+  photo: PropTypes.object.isRequired,
+  showReviewModal: PropTypes.func,
+  id: PropTypes.number,
+};
+
 
 export default GalleryModalPicture;
